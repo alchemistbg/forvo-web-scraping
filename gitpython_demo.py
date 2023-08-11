23ba4238295d970d19f357cb52bbb507d6c4703b
alchemistbg <alchemist_bg@abv.bg>
2023-08-11 20:18:22 +0200

    Add demofile for playing with gitpython

--- gitpython_demo.py
+++ gitpython_demo.py
@@ -1,7 +1,7 @@
 import git
 
 repo = git.Repo(".")
-repo.remotes.origin.set_url("git@github.com:alchemistbg/forvo-web-scraping.git")
+repo.remotes.origin.set_url("ssh://git@github.com:alchemistbg/forvo-web-scraping.git")
 repo.git.add(".")
 repo.git.commit(m = "Add demofile for playing with gitpython")
 repo.git.push()
